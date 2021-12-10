1fdd309c26c54d35c0273237f9837c0a0f49acd5
onskh <onskhardano1@gmail.com>
2021-12-10 06:53:27 +0100

    home button

---
+++ main.js
@@ -0,0 +1,287 @@
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+var header1 = $("<h2>Aries</h2>")
+$(".one").append(header1)
+
+var p1 = $("<p>Aries is the second sign of the Zodiac and is associated with material pleasure. Individuals born under this sign are thought to have a calm, patient, reliable, loyal, affectionate, sensuous, ambitious, and determined character. <br> They are also prone to hedonism, laziness, inflexibility, jealousy, and antipathy. </p>")
+$(".one").append(p1)
+
+var image1= $("<img>")
+image1.attr("src","./pictures/aries.jpg")
+$(".one").append(image1)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+var header2 = $("<h2>Taurus</h2>")
+$(".two").append(header2)
+
+var p2 = $("<p>Taurus is the second sign of the Zodiac and is associated with material pleasure. Individuals born under this sign are thought to have a calm, patient, reliable, loyal, affectionate, sensuous, ambitious, and determined character. <br> They are also prone to hedonism, laziness, inflexibility, jealousy, and antipathy. </p>")
+$(".two").append(p2)
+
+var image2= $("<img>")
+image2.attr("src","./pictures/taurus.jpg")
+$(".two").append(image2)
+/////////////////////////////////////////////////////////
+
+var header3 = $("<h2>Gemini</h2>")
+$(".three").append(header3)
+
+var p3 = $("<p> Gemini is the third sign of the Zodiac and is associated with youth and versatility. Individuals born under this sign are thought to have a sociable, fun-loving, versatile, lively, communicative, liberal, intelligent, mentally active, and friendly character. <br> They are also thought to be prone to moodiness, inconsistency, superficiality, restlessness, and laziness.</p>")
+$(".three").append(p3)
+
+var image3= $("<img>")
+image3.attr("src","./pictures/gemini.jpg")
+$(".three").append(image3)
+//////////////////////////////////////////////////////////////
+
+var header4 = $("<h2>Cancer</h2>")
+$(".four").append(header4)
+
+var p4 = $("<p>Cancer is the fourth sign of the Zodiac. It is associated with family and domesticity. Individuals born under this sign are thought to have a kind, emotional, romantic, imaginative, sympathetic, nurturing, and intuitive character. <br> They are also supposed to be prone to changeability, moodiness, hypersensitivity, depression, and clinginess.</p>")
+$(".four").append(p4)
+
+var image4= $("<img>")
+image4.attr("src","./pictures/cancer.jpg")
+$(".four").append(image4)
+/////////////////////////////////////////////////////////
+
+var header5 = $("<h2>Leo</h2>")
+$(".five").append(header5)
+
+var p5 = $("<p> Leo is the fifth sign of the Zodiac and is associated with the keywords magnanimous, generous, hospitable, caring, warm, authoritative, active, and open. Leos are typically pictured as very dignified and regal. They are hard-working, ambitious, and enthusiastic, however, they are supposedly prone to laziness, often choosing to take the easy way out. They are known to be exuberant, extroverted, and generous. They have a natural dramatic flair and are very creative. <br> They are typically very self-assured and love taking center-stage in whatever arena they are in.</p>")
+$(".five").append(p5)
+
+var image5= $("<img>")
+image5.attr("src","./pictures/leo.jpg")
+$(".five").append(image5)
+/////////////////////////////////////////////////////////////
+
+var header6 = $("<h2>Virgo</h2>")
+$(".six").append(header6)
+
+var p6 = $("<p> Virgo is the sixth sign of the Zodiac. It is associated with purity and service. Individuals born under this sign are thought to have a diligent, analytical, self-sufficient, controlled, orderly, and modest character. <br> But they are also prone to fussiness, perfectionism, harsh criticism, coldness, and hypochondria.</p>")
+$(".six").append(p6)
+
+var image6= $("<img>")
+image6.attr("src","./pictures/virgo.jpg")
+$(".six").append(image6)
+/////////////////////////////////////////////////
+
+var header7 = $("<h2>Libra</h2>")
+$(".seven").append(header7)
+
+var p7 = $("<p>Libra is the seventh sign of the Zodiac. It is associated with justice. Individuals born under this sign are thought to have a pleasant, articulate, charming, social, charismatic character. They are artistic. But they also possess a fair, refined, diplomatic, even-tempered and self-sufficient character. On the negative side, they are thought to be indecisive, lazy, aloof, flirtatious, and shallow. <br> They are also supposedly extravagant, frivolous, impatient, envious, and quarrelsome.</p>")
+$(".seven").append(p7)
+
+var image7= $("<img>")
+image7.attr("src","./pictures/libra.jpg")
+$(".seven").append(image7)
+///////////////////////////////////////////////////////////
+var header8 = $("<h2>Scorpius</h2>")
+$(".eight").append(header8)
+
+var p8 = $("<p>  Scorpio is the eighth sign of the Zodiac. It is associated with intensity, passion, and power. Individuals born under this sign are thought to have a complex, analytical, patient, keenly perceptive, inquisitive, focused, determined, hypnotic, and self-contained character. <br> They are also prone to extremity, jealousy, envy, secretiveness, possessiveness, cruelty, and cunning.</p>")
+$(".eight").append(p8)
+
+var image8= $("<img>")
+image8.attr("src","./pictures/scorpio.jpg")
+$(".eight").append(image8)
+////////////////////////////////////////////////////
+var header9 = $("<h2>Sagittarius</h2>")
+$(".nine").append(header9)
+
+var p9 = $("<p> Sagittarius is the ninth sign of the Zodiac. It is associated with travel and expansion. Individuals born under this sign are thought to have a straight-forward, dynamic, highly intelligent, extremely clever, ethical, humorous, generous, open-hearted, compassionate, and energetic character. <br> They are also prone to restlessness, impulsiveness, impatience, and recklessness.</p>")
+$(".nine").append(p9)
+
+var image9= $("<img>")
+image9.attr("src","./pictures/sagittarius.jpg")
+$(".nine").append(image9)
+/////////////////////////////////////////////////
+var header10 = $("<h2>Capricorn</h2>")
+$(".ten").append(header10)
+
+var p10 = $("<p>Capricorn is the 10th sign of the Zodiac and is associated with hard work and business affairs. Individuals born under this sign are thought to have an ambitious, modest, patient, responsible, stable, trustworthy, powerful, intellectual, perspicacious, and persistent character. <br> They are also prone to coldness, conservatism, rigidity, materialism, and dullness.</p>")
+$(".ten").append(p10)
+
+var image10= $("<img>")
+image10.attr("src","./pictures/capricorn.jpg")
+$(".ten").append(image10)
+//////////////////////////////////////////////////////////
+var header11 = $("<h2>Aquarius</h2>")
+$(".eleven").append(header11)
+
+var p11 = $("<p>Aquarius is the 11th sign of the Zodiac and is associated with future ideas and the unusual. Individuals born under this sign are thought to have a modest, creative, challenging, inquisitive, entertaining, progressive, stimulating, nocturnal, and independent character. <br> They are also prone to rebelliousness, coldness, erraticism, indecisiveness, and impracticality.</p>")
+$(".eleven").append(p11)
+
+var image11= $("<img>")
+image11.attr("src","./pictures/aquarius.jpg")
+$(".eleven").append(image11)
+////////////////////////////////////////////////////:
+var header12 = $("<h2>Pisces</h2>")
+$(".twelve").append(header12)
+
+var p12 = $("<p> Pisces is the 12th and last sign of the Zodiac and is associated with human emotions. Individuals born under this sign are thought to be tolerant, modest, dreamy, romantic, humorous, generous, emotional, receptive, and affectionate. They are thought to have an honest character. <br> But they are also prone to exaggeration, fickleness, passiveness, hypersensitivity, and paranoia.</p>")
+$(".twelve").append(p12)
+
+var image12= $("<img>")
+image12.attr("src","./pictures/pisces.jpg")
+$(".twelve").append(image12)
+////////////////////////////////////////////////////////////
+
+$("document").ready(function(){
+    $(".one").hide()
+
+    $(".two").hide()
+
+    $(".three").hide()
+
+    $(".four").hide()
+    
+    $(".five").hide()
+
+    $(".six").hide()
+
+    $(".seven").hide()
+
+    $(".eight").hide()
+
+    $(".nine").hide()
+
+    $(".ten").hide()
+
+    $(".eleven").hide()
+
+    $(".twelve").hide()
+})
+
+////////////////////////////////////////////////////////////////////////////
+
+var btn = $("#btn")
+$("document").ready(function(){
+    btn.on('click', function(){
+        $(".one").show()
+    })
+})
+
+var btn1 = $("#btn1")
+$("document").ready(function(){
+    btn1.on('click', function(){
+        $(".two").show()
+    })
+})
+
+var btn2 = $("#btn2")
+$("document").ready(function(){
+    btn2.on('click', function(){
+        $(".three").show()
+    })
+})
+
+var btn3 = $("#btn3")
+$("document").ready(function(){
+    btn3.on('click', function(){
+        $(".four").show()
+    })
+})
+
+var btn4 = $("#btn4")
+$("document").ready(function(){
+    btn4.on('click', function(){
+        $(".five").show()
+    })
+})
+
+var btn5 = $("#btn5")
+$("document").ready(function(){
+    btn5.on('click', function(){
+        $(".six").show()
+    })
+})
+
+var btn6 = $("#btn6")
+$("document").ready(function(){
+    btn6.on('click', function(){
+        $(".seven").show()
+    })
+})
+
+var btn7 = $("#btn7")
+$("document").ready(function(){
+    btn7.on('click', function(){
+        $(".eight").show()
+    })
+})
+
+var btn8 = $("#btn8")
+$("document").ready(function(){
+    btn8.on('click', function(){
+        $(".nine").show()
+    })
+})
+
+var btn9 = $("#btn9")
+$("document").ready(function(){
+    btn9.on('click', function(){
+        $(".ten").show()
+    })
+})
+
+var btn10 = $("#btn10")
+$("document").ready(function(){
+    btn10.on('click', function(){
+        $(".eleven").show()
+    })
+})
+
+var btn11 = $("#btn11")
+$("document").ready(function(){
+    btn11.on('click', function(){
+        $(".twelve").show()
+    })
+})
+
+/////////////////////////////////////////////////////////////////////////
+
+var home = $('#home')
+$("document").ready(function(){
+    home.on('click', function(){
+
+$(".one").hide()
+
+$(".two").hide()
+
+$(".three").hide()
+
+$(".four").hide()
+
+$(".five").hide()
+
+$(".six").hide()
+
+$(".seven").hide()
+
+$(".eight").hide()
+
+$(".nine").hide()
+
+$(".ten").hide()
+
+$(".eleven").hide()
+
+$(".twelve").hide()
+btn.show()
+btn1.show()
+btn2.show()
+btn3.show()
+btn4.show()
+btn5.show()
+btn6.show()
+btn7.show()
+btn8.show()
+btn9.show()
+btn10.show()
+btn11.show()
+    })
+})
+    
+    
